f921a564dff91cec65d36a3073888b3b0c690564
Michael Tang <michaeltang@Michaels-MacBook-Pro.local>
2016-08-21 16:27:20 -0600

    Add count and insertAtIndex

--- LinkedList.js
+++ LinkedList.js
@@ -27,6 +27,48 @@ function pop(list){
     temp.next = null;
 }
 
+//count number of nodes in list
+function count(list){
+    var count = 0;
+    var temp = list;
+    while(temp != null){
+        temp = temp.next;
+        count++;
+    }
+    return count;
+}
+
+//Insert node at index, function takes the list, the node to be placed, and index as arguments. Function returns the list
+function insertAtIndex(list,x,index){
+    if(list == null){
+        list = x;
+        return list;
+    }
+    
+    if(index == 0){
+        var newList = x;
+        x.next = list;
+        return newList;
+    }
+    
+    if(index > count(list) || index < 0){
+        print("Error: Indexed out of bounds");
+        return list;
+    }
+    
+    var temp = list;
+    var counts = 1;
+    
+    while(temp != null && (counts < index)){
+        temp = temp.next;
+        counts++;
+    }
+    var pointer = temp.next;
+    temp.next = x;
+    x.next = pointer;
+    return list;
+}
+
 function printList(list){
     var temp = list;
     while(temp != null){
@@ -47,7 +89,16 @@ function main(){
     
     // printList(head);
     
+    // Remove last node
     // pop(head);
+    
+    // var four = new LinkedListNode("Me first!");
+    
+    // Insert node at index
+    // head = insertAtIndex(head,four,0);
+    
+    // printList(head);
+    
 }
 
 main();
